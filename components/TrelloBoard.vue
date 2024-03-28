75db37342621920846965336eab49279485ad5e5
Wallace Bruno <wallace.bruno@geocode.com.br>
2024-03-28 00:19:37 -0300

    create add and remove tasks

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,6 +1,46 @@
+<template>
+  <div>
+    <draggable
+      v-model="columns"
+      group="columns"
+      :animation="150"
+      handle=".drag-handle"
+      item-key="id"
+      class="flex gap-4 overflow-x-auto items-start"
+    >
+      <template #item="{ element: column }: { element: Column }">
+        <div class="column bg-gray-200 p-5 rounded min-w-[250px] mb-5">
+          <header class="font-bold mb-4">
+            <DragHandle />
+            {{ column.title }}
+          </header>
+          <draggable
+            v-model="column.tasks"
+            :group="{name: 'tasks', pull: alt ? 'clone' : true}"
+            :animation="150"
+            handle=".drag-handle"
+            item-key="id"
+          >
+            <template #item="{ element: task }: { element: Task }">
+              <div>
+                <TrelloBoardTask :task="task" @delete="column.tasks = column.tasks.filter(t => t.id !== $event)"/>
+              </div>
+            </template>
+          </draggable>
+          <footer>
+            <NewTask @add="column.tasks.push($event)"/>
+          </footer>
+        </div>
+      </template>
+    </draggable>
+  </div>
+</template>
+
 <script setup lang="ts">
-import type { Column } from "~/types";
+import type { Column, Task } from "~/types";
+import draggable from "vuedraggable";
 import { nanoid } from "nanoid";
+
 const columns = ref<Column[]>([
   {
     id: nanoid(),
@@ -49,25 +89,6 @@ const columns = ref<Column[]>([
     tasks: [],
   },
 ]);
+const alt = useKeyModifier("Alt")
 </script>
-<template>
-  <div class="flex gap-4 overflow-x-auto items-start">
-    <div
-      v-for="column in columns"
-      :key="column.id"
-      class="column bg-gray-200 p-5 rounded min-w-[250px]"
-    >
-      <header class="font-bold mb-4">
-        {{ column.title }}
-      </header>
-      <TrelloBoardTask
-        v-for="task in column.tasks"
-        :task="task"
-        :key="task.id"
-      />
-      <footer>
-        <button class="text-gray-500">+ Add a Card</button>
-      </footer>
-    </div>
-  </div>
-</template>
+
